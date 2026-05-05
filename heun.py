422d092333d2e2b27d2b3486e4a0f42801333a7e
Michael <michael.udemba@manchester.ac.uk>
2026-05-05 15:59:26 +0100

    more linting and made heun.py work as a standalone module

--- heun.py
+++ heun.py
@@ -20,10 +20,10 @@ def weight_func(z_range, a, gamma, delta, epsilon):
     return (z_range**gamma) * ((z_range - 1)**delta) * ((a - z_range)**epsilon) * np.exp(z_range)
 
 
-def get_kernel_1(x_vec, y_vec, delta_z) -> np.ndarray:
+def get_kernel_1(x_vec, y_vec, delta_z, points) -> np.ndarray:
     """Returns an array which is the matrix form of K_1"""
     integrand = x_vec * y_vec
-    consecutive_integrals = 0.5*(integrand[0:N-1] + integrand[1:N])
+    consecutive_integrals = 0.5*(integrand[0:points-1] + integrand[1:points])
     consecutive_integrals = np.insert(consecutive_integrals, 0, 0, axis=0)
 
     cumulative_sums = np.cumsum(consecutive_integrals)
@@ -39,81 +39,86 @@ def get_kernel_1(x_vec, y_vec, delta_z) -> np.ndarray:
     return kernel
 
 
-def get_kernel_2(x_vec, q_vec, z_range) -> np.ndarray:
+def get_kernel_2(x_vec, q_vec, z_range, points) -> np.ndarray:
     """Returns an array which is the matrix form of K_2"""
-    kernel = np.zeros((N, N), dtype=complex)
+    kernel = np.zeros((points, points), dtype=complex)
 
     # Can we vectorise this?
-    for i in range(N):
+    for i in range(points):
         kernel[i] = np.real(np.exp(-z_range)*np.exp(z_range[i])*x_vec + q_vec)
 
     return kernel
 
 
-def get_neumann_sum(matrix_kernel, delta_z):
+def get_neumann_sum(matrix_kernel, delta_z, points):
     """Returns"""
     diagonal = np.diag(np.diag(matrix_kernel))
-    id = np.identity(N)
+    identity = np.identity(points)
+    v = np.zeros(points)
+    v[0] = 1
 
-    lhs = IDENTITY_MATRIX - delta_z*matrix_kernel + 0.5*delta_z*diagonal
+    lhs = identity - delta_z*matrix_kernel + 0.5*delta_z*diagonal
 
-    neumann_sum = (1/delta_z)*(solve_triangular(lhs, V, trans=1))
+    neumann_sum = (1/delta_z)*(solve_triangular(lhs, v, trans=1))
 
     neumann_sum[0] = 0
 
     return neumann_sum
 
 
-def path_ordered_exp_1(x_vec: np.ndarray, y_vec: np.ndarray, delta_z: float, H0) -> np.ndarray:
+def path_ordered_exp_1(x_vec: np.ndarray, y_vec: np.ndarray, delta_z: float,
+                       init_val, points) -> np.ndarray:
     """Returns the contribution to the path-ordered exponential from K_1"""
-    kernel_1 = get_kernel_1(x_vec, y_vec, delta_z)
-    green_1 = get_neumann_sum(kernel_1, delta_z)
+    kernel_1 = get_kernel_1(x_vec, y_vec, delta_z, points)
+    green_1 = get_neumann_sum(kernel_1, delta_z, points)
 
-    int_g_1 = 0.5*delta_z*(green_1[0:N - 1] + green_1[1:N])
+    int_g_1 = 0.5*delta_z*(green_1[0:points - 1] + green_1[1:points])
     int_g_1 = np.insert(int_g_1, 0, 0, axis=0)
     int_g_1 = np.cumsum(int_g_1)
-    return H0*(1 + int_g_1)
+    return init_val*(1 + int_g_1)
 
 
 def path_ordered_exp_2(q_vec: np.ndarray, x_vec: np.ndarray,
-                       delta_z: float, z_range: np.ndarray, H0, H0_PRIME) -> np.ndarray:
+                       delta_z: float, z_range: np.ndarray,
+                       init_val, init_slope, points) -> np.ndarray:
     """Returns the contribution to the path-ordered exponential from K_2"""
-    kernel_2 = get_kernel_2(x_vec, q_vec, z_range)
-    green_2 = get_neumann_sum(kernel_2, delta_z)
+    kernel_2 = get_kernel_2(x_vec, q_vec, z_range, points)
+    green_2 = get_neumann_sum(kernel_2, delta_z, points)
 
     exp_g_2 = np.exp(-z_range)*green_2
-    sum_exp_g_2 = 0.5*delta_z*(exp_g_2[1:N]+exp_g_2[0:N-1])
+    sum_exp_g_2 = 0.5*delta_z*(exp_g_2[1:points]+exp_g_2[0:points-1])
     sum_exp_g_2 = np.insert(sum_exp_g_2, 0, 0, axis=0)
     sum_exp_g_2 = np.cumsum(sum_exp_g_2)
     res_g_2 = np.exp(z_range)*sum_exp_g_2
 
-    int_g_2 = 0.5*delta_z*(green_2[1:N] + green_2[0:N-1])
+    int_g_2 = 0.5*delta_z*(green_2[1:points] + green_2[0:points-1])
     int_g_2 = np.insert(int_g_2, 0, 0, axis=0)
     int_g_2 = np.cumsum(int_g_2)
 
     res_2 = np.exp(z_range-z_range[0]) - 1 + res_g_2 - int_g_2
 
-    return (H0_PRIME - H0) * res_2
+    return (init_slope - init_val) * res_2
 
 
 def path_sum(init_conditions: list, params: list, z_range: np.ndarray) -> np.ndarray:
     """Returns the R matrix, whose first column approximates the solution to the Heun equation"""
     a, q = params[0], params[1]
     alpha, beta, gamma, delta = params[2], params[3], params[4], params[5]
-    epsilon = alpha + beta + 1 - gamma - delta
+    epsilon = 1 + alpha + beta - gamma - delta
 
-    N = len(z_range)
-    H0, H0_PRIME = init_conditions[0], init_conditions[1]
-    delta_z = (z_range[-1] - z_range[0])/(N - 1)
+    points = len(z_range)
+    init_val, init_slope = init_conditions[0], init_conditions[1]
+    delta_z = (z_range[-1] - z_range[0])/(points - 1)
 
     p_func = heun_eq_coeff_1(z_range, a, gamma, delta, epsilon)
     q_func = heun_eq_coeff_0(z_range, a, q, alpha, beta)
     x_func = - p_func - q_func - 1
     y_func = weight_func(z_range, a, gamma, delta, epsilon)
 
-    heun_function = path_ordered_exp_1(x_func, y_func, delta_z, H0)
+    heun_function = path_ordered_exp_1(
+        x_func, y_func, delta_z, init_val, points)
 
     heun_function += path_ordered_exp_2(q_func,
-                                        x_func, delta_z, z_range, H0, H0_PRIME)
+                                        x_func, delta_z, z_range, init_val, init_slope, points)
 
     return heun_function

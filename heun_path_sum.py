24310bb54987f9a5fcd4d01bd386a825fe641777
Michael <michael.udemba@manchester.ac.uk>
2026-05-06 13:07:12 +0100

    plotting functionality restored, there is still a logic error lurking

--- heun_path_sum.py
+++ heun_path_sum.py
@@ -25,7 +25,8 @@ def get_kernel_1(x_vec, y_vec, delta_z) -> np.ndarray:
     """Returns an array which is the matrix form of K_1"""
     integrand = x_vec * y_vec
 
-    cumulative_sums = cumulative_trapezoid(integrand, dx=delta_z, initial=0)
+    cumulative_sums = cumulative_trapezoid(
+        integrand, dx=delta_z, axis=0, initial=0)
 
     kx, ky = np.meshgrid(cumulative_sums, cumulative_sums, sparse=False)
 
@@ -54,11 +55,11 @@ def get_neumann_sum(matrix_kernel, delta_z, points):
     """Returns"""
     diagonal = np.diag(np.diag(matrix_kernel))
     identity = np.identity(points)
-    v = np.transpose(identity[0])
+    v = identity[0]
 
     lhs = identity - delta_z*matrix_kernel + 0.5*delta_z*diagonal
 
-    neumann_sum = (1/delta_z)*(solve_triangular(lhs, v) - v)
+    neumann_sum = (1/delta_z)*(solve_triangular(lhs, v, trans=1) - v)
 
     neumann_sum[0] = 0
 
@@ -71,8 +72,9 @@ def path_ordered_exp_1(x_vec: np.ndarray, y_vec: np.ndarray, delta_z: float, poi
     kernel = get_kernel_1(x_vec, y_vec, delta_z)
     green = get_neumann_sum(kernel, delta_z, points)
 
-    integral_of_green = cumulative_trapezoid(green, dx=delta_z, initial=0)
-    return 1 + integral_of_green
+    integral_of_green = cumulative_trapezoid(
+        green, dx=delta_z, axis=0, initial=0)
+    return integral_of_green
 
 
 def path_ordered_exp_2(q_vec: np.ndarray, x_vec: np.ndarray,
@@ -83,14 +85,10 @@ def path_ordered_exp_2(q_vec: np.ndarray, x_vec: np.ndarray,
     green_2 = get_neumann_sum(kernel_2, delta_z, points)
 
     exp_g_2 = np.exp(-z_range)*green_2
-    sum_exp_g_2 = 0.5*delta_z*(exp_g_2[1:points]+exp_g_2[0:points-1])
-    sum_exp_g_2 = np.insert(sum_exp_g_2, 0, 0, axis=0)
-    sum_exp_g_2 = np.cumsum(sum_exp_g_2)
+    sum_exp_g_2 = cumulative_trapezoid(exp_g_2, dx=delta_z, axis=0, initial=0)
     res_g_2 = np.exp(z_range)*sum_exp_g_2
 
-    int_g_2 = 0.5*delta_z*(green_2[1:points] + green_2[0:points-1])
-    int_g_2 = np.insert(int_g_2, 0, 0, axis=0)
-    int_g_2 = np.cumsum(int_g_2)
+    int_g_2 = cumulative_trapezoid(green_2, dx=delta_z, axis=0, initial=0)
 
     res_2 = np.exp(z_range-z_range[0]) - 1 + res_g_2 - int_g_2
 

bc46b0ba448695e4908c0ba800ee0f095fb7ed0c
Michael <michael.udemba@manchester.ac.uk>
2026-05-06 15:03:05 +0100

    added estimate of h0 and h0_prime at z0 value near 0

--- heun_path_sum.py
+++ heun_path_sum.py
@@ -18,19 +18,19 @@ def heun_eq_coeff_0(z_range: np.ndarray,
     return (alpha * beta * z_range - q) / (z_range * (z_range - 1) * (z_range - a))
 
 
-def weight_func(z_range, a, gamma, delta, epsilon):
+def weight_func(z_range: np.ndarray,
+                a: complex, gamma: complex, delta: complex, epsilon: complex) -> np.ndarray:
     """The factor in the integrand of J(z) which precedes X(z)"""
     return (z_range**gamma) * ((z_range - 1)**delta) * ((a - z_range)**epsilon) * np.exp(z_range)
 
 
-def get_kernel_1(x_vec, y_vec, delta_z) -> np.ndarray:
+def get_kernel_1(x_vec: np.ndarray, y_vec: np.ndarray, delta_z: float) -> np.ndarray:
     """Returns an array which is the matrix form of K_1"""
     integrand = x_vec * y_vec
 
-    cumulative_sums = cumulative_trapezoid(
-        integrand, dx=delta_z, axis=0, initial=0)
+    cumulative_traps = cumulative_trapezoid(integrand, dx=delta_z, initial=0)
 
-    kx, ky = np.meshgrid(cumulative_sums, cumulative_sums, sparse=False)
+    kx, ky = np.meshgrid(cumulative_traps, cumulative_traps, sparse=False)
 
     kernel = kx - ky
 
@@ -41,50 +41,51 @@ def get_kernel_1(x_vec, y_vec, delta_z) -> np.ndarray:
     return kernel
 
 
-def get_kernel_2(x_vec, q_vec, z_range) -> np.ndarray:
+def get_kernel_2(x_vec: np.ndarray, q_vec: np.ndarray, z_range: np.ndarray) -> np.ndarray:
     """Returns an array which is the matrix form of K_2"""
     exp_vec = np.exp(z_range)
 
-    x_times_e, inv_exp = np.meshgrid(x_vec * exp_vec, 1/exp_vec, sparse=False)
+    x_times_e, exp = np.meshgrid(x_vec * exp_vec, exp_vec, sparse=False)
     q_rows, _ = np.meshgrid(q_vec, q_vec, sparse=False)
 
-    kernel = x_times_e*inv_exp + q_rows
+    kernel = x_times_e/exp + q_rows
 
     return kernel
 
 
-def get_neumann_sum(matrix_kernel, delta_z, points):
-    """Returns"""
+def neumann_sum(matrix_kernel: np.ndarray, delta_z: float, points: int) -> np.ndarray:
+    """Returns the Neumann series of the kernel matrix_kernel,
+    which is equivalent to the inverse *-resolvent of 1 - kernel"""
     diagonal = np.diag(np.diag(matrix_kernel))
     identity = np.identity(points)
     v = identity[0]
 
     lhs = identity - delta_z*matrix_kernel + 0.5*delta_z*diagonal
 
-    neumann_sum = (1/delta_z)*(solve_triangular(lhs, v, trans=1) - v)
+    n_sum = (1/delta_z)*(solve_triangular(lhs, v, trans=1) - v)
 
-    neumann_sum[0] = 0
+    n_sum[0] = 0
 
-    return neumann_sum
+    return n_sum
 
 
-def path_ordered_exp_1(x_vec: np.ndarray, y_vec: np.ndarray, delta_z: float, points) -> np.ndarray:
+def path_ordered_exp_1(x_vec: np.ndarray, y_vec: np.ndarray, delta_z: float, points: int) -> np.ndarray:
     """Returns the first contribution to the path-ordered exponential.
     Computation chain: kernel K_1 -> Green's function G_1 -> path-ordered exponential U_11"""
     kernel = get_kernel_1(x_vec, y_vec, delta_z)
-    green = get_neumann_sum(kernel, delta_z, points)
+    green = neumann_sum(kernel, delta_z, points)
 
     integral_of_green = cumulative_trapezoid(
         green, dx=delta_z, axis=0, initial=0)
-    return integral_of_green
+    return 1 + integral_of_green
 
 
 def path_ordered_exp_2(q_vec: np.ndarray, x_vec: np.ndarray,
-                       delta_z: float, z_range: np.ndarray, points) -> np.ndarray:
+                       delta_z: float, z_range: np.ndarray, points: int) -> np.ndarray:
     """Returns the second contribution to the path-ordered exponential.
     Computation chain: kernel K_2 -> Green's function G_2 -> path-ordered exponential U_12"""
     kernel_2 = get_kernel_2(x_vec, q_vec, z_range)
-    green_2 = get_neumann_sum(kernel_2, delta_z, points)
+    green_2 = neumann_sum(kernel_2, delta_z, points)
 
     exp_g_2 = np.exp(-z_range)*green_2
     sum_exp_g_2 = cumulative_trapezoid(exp_g_2, dx=delta_z, axis=0, initial=0)
@@ -102,10 +103,14 @@ def heun(z_range: np.ndarray, *, a: complex, q: complex,
     """Returns the R matrix, whose first column approximates the solution to the Heun equation"""
     epsilon = 1 + alpha + beta - gamma - delta
 
+    z0 = z_range[0]
     points = len(z_range)
-    delta_z = (z_range[-1] - z_range[0])/(points - 1)
-    direction = np.sign(z_range[-1] - z_range[0])
-    init_val, init_slope = 1, direction*q/(gamma*a)
+    delta_z = (z_range[-1] - z0)/(points - 1)
+    direction = np.sign(z_range[-1] - z0)
+    init_val = 1 + q*z0/(gamma*a) + (z0**2)*(q**2 - a*alpha*beta +
+                                             q*(1+alpha+beta+a*gamma+delta*(a-1)))/(2*a**2*gamma*(1+gamma))
+    init_slope = q/(gamma*a) + z0*(q**2 - a*alpha*beta +
+                                   q*(1+alpha+beta+a*gamma+delta*(a-1)))/(a**2*gamma*(1+gamma))
 
     p_func = heun_eq_coeff_1(z_range, a, gamma, delta, epsilon)
     q_func = heun_eq_coeff_0(z_range, a, q, alpha, beta)
@@ -127,21 +132,19 @@ if __name__ == "__main__":
     Q = -0.2 + 1j*0
     ALPHA = 1.3 + 1j*0
     BETA = 0.12 + 1j*0
-    GAMMA = -0.4 + 1j*0
+    GAMMA = -0.14 + 1j*0
     DELTA = 4.32 + 1j*0
 
     # Domain definition
-    N = 1000
+    N = 100
     BUFFER = 1e-11
 
-    Z_MIN = 0 + BUFFER
-    # z_max = 8.64359*1e6
+    Z_MIN = 0.1
     Z_MAX = 0.5
     Z = np.linspace(Z_MIN, Z_MAX, N)
 
     start = time.perf_counter()
-    y = heun(Z, a=A, q=Q, alpha=ALPHA,
-             beta=BETA, gamma=GAMMA, delta=DELTA)
+    y = heun(Z, a=A, q=Q, alpha=ALPHA, beta=BETA, gamma=GAMMA, delta=DELTA)
     end = time.perf_counter()
 
     print(end - start)

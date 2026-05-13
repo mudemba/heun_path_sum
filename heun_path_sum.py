attempting to locate the source of the derivative bug

heun_path_sum.py
@@ -135,6 +135,8 @@ def heun(z_range: np.ndarray, *, a: complex, q: complex,
 
     subintervals = subdivide_domain(z_range)
     heun_function = np.array([init_val])
+
+    print(init_val + delta_z*init_slope)
     for subinterval in subintervals:
         points = len(subinterval)
         p_func = heun_eq_coeff_1(subinterval, a, gamma, delta, epsilon)
@@ -150,8 +152,7 @@ def heun(z_range: np.ndarray, *, a: complex, q: complex,
         heun_function = np.append(heun_function, contribution[1:])
         init_val = contribution[-1]
         init_slope = (contribution[-1] - contribution[-2])/delta_z
-        # print(init_slope)
-
+    print(heun_function[1])
     return heun_function
 
 
@@ -166,7 +167,7 @@ if __name__ == "__main__":
     DELTA = 0.5 + 1j*0
 
     # Domain definition
-    N = 100000
+    N = 10000
 
     Z_MIN = 1.001
     # Z_MAX = 8.64359*1e6